33b7762fbc74ba7b434f766dfebb897cda1f8856
Gon <rojas.gonzalo7@gmail.com>
2020-07-12 00:37:58 -0400

    Docs agregado

---
+++ docs/precache-manifest.13f9c6011aa5cf71fdaf05240bf94eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1872eb3220d69b27c007bd23fa06ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b2af5926d328765c26e",
+    "url": "/static/css/main.0a5a2069.chunk.css"
+  },
+  {
+    "revision": "813340125952adac502d",
+    "url": "/static/js/2.8f5b0e35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f5b0e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2af5926d328765c26e",
+    "url": "/static/js/main.c7468316.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
